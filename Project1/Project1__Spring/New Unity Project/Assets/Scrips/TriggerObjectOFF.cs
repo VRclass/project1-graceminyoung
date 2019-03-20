0969f3f19487ff107245f3652c4d8877c23e11c8
GraceMint <46830892+graceminyoung@users.noreply.github.com>
2019-03-19 21:40:10 -0500

    1
    1

---
+++ Project1/Project1__Spring/New Unity Project/Assets/Scrips/TriggerObjectOFF.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerObjectOFF : MonoBehaviour
+{
+    public Light ligthToSwitch = null;
+
+    // When the trigger is entered 
+    public void OnTriggerEnter(Collider other)
+    {
+        ligthToSwitch.enabled = false; // turn the light off
+        print("entered the OFF trigger");  // print the message
+    }
+
+}
